3642e3fb5e0633d402dbb5e50d43a9fe53e89a1b
Manthan Sharma <manthan2306s@gmail.com>
2024-07-30 14:51:55 +0530

    add movie review

--- programs/anchor-movie-review-program/src/lib.rs
+++ programs/anchor-movie-review-program/src/lib.rs
@@ -6,11 +6,62 @@ declare_id!("BZRT2WuQaBUkRJkFs8yWdph2PNdt7pKzjdyJt9Pd6KLR");
 pub mod anchor_movie_review_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn add_movie_review_program(
+        ctx: Context<AddMovieReview>,
+        title: String,
+        description: String,
+        rating: u8,
+    ) -> Result<()> {
+        msg!("Movie Review Acccount Created");
+        msg!("Title : {}", title);
+        msg!("Description : {}", description);
+        msg!("Rating : {}", rating);
+
+        let movie_review = &mut ctx.accounts.movie_review;
+
+        movie_review.title = title;
+        movie_review.description = description;
+        movie_review.rating = rating;
+        movie_review.reviewer = ctx.accounts.initializer.key();
+
+        Ok(())
+    }
+
+    pub fn update_movie_review(ctx: Context<UpdateMovieReview>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(title:String,description:String)]
+pub struct AddMovieReview<'info> {
+    #[account(
+        init,
+        seeds=[title.as_bytes(),initializer.key().as_ref()],
+        bump,
+        payer=initializer,
+        space=MovieAccountState::INIT_SPACE+title.len()+description.len()
+    )]
+    pub movie_review: Account<'info, MovieAccountState>,
+
+    #[account(mut)]
+    initializer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(title:String,description:String)]
+pub struct UpdateMovieReview {}
+
+#[account]
+pub struct MovieAccountState {
+    pub reviewer: Pubkey,
+    pub rating: u8,
+    pub title: String,
+    pub description: String,
+}
+
+impl Space for MovieAccountState {
+    const INIT_SPACE: usize = 8 + 32 + 1 + 4 + 4;
+}
