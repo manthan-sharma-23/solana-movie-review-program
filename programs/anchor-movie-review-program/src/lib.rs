cccc49d1b0deca96323a8fb38b5e8d1b7c659a88
Manthan Sharma <manthan2306s@gmail.com>
2024-07-30 15:03:26 +0530

    movie review update delete

--- programs/anchor-movie-review-program/src/lib.rs
+++ programs/anchor-movie-review-program/src/lib.rs
@@ -27,7 +27,27 @@ pub mod anchor_movie_review_program {
         Ok(())
     }
 
-    pub fn update_movie_review(ctx: Context<UpdateMovieReview>) -> Result<()> {
+    pub fn update_movie_review(
+        ctx: Context<UpdateMovieReview>,
+        title: String,
+        description: String,
+        rating: u8,
+    ) -> Result<()> {
+        msg!("Movie review account space reallocated");
+        msg!("Title: {}", title);
+        msg!("Description: {}", description);
+        msg!("Rating: {}", rating);
+
+        let movie_review = &mut ctx.accounts.movie_review;
+        movie_review.title = title;
+        movie_review.description = description;
+        movie_review.rating = rating;
+
+        Ok(())
+    }
+
+    pub fn delete_movie_review(_ctx: Context<DeleteMovieReview>, title: String) -> Result<()> {
+        msg!("Movie Review Deleted {}", title);
         Ok(())
     }
 }
@@ -52,7 +72,37 @@ pub struct AddMovieReview<'info> {
 
 #[derive(Accounts)]
 #[instruction(title:String,description:String)]
-pub struct UpdateMovieReview {}
+pub struct UpdateMovieReview<'info> {
+    #[account(
+        mut,
+        seeds=[title.as_bytes(),initializer.key().as_ref()],
+        bump,
+        realloc=MovieAccountState::INIT_SPACE+title.len()+description.len(),
+        realloc::payer=initializer,
+        realloc::zero=true
+    )]
+    pub movie_review: Account<'info, MovieAccountState>,
+
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(title:String)]
+pub struct DeleteMovieReview<'info> {
+    #[account(
+        mut,
+        seeds=[title.as_bytes(),initializer.key().as_ref()],
+        bump,
+        close=initializer
+    )]
+    pub movie_review: Account<'info, MovieAccountState>,
+
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
 
 #[account]
 pub struct MovieAccountState {
